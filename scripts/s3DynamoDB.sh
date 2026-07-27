1e132f614bfae516a9d7ed52b0af08fa97eef344
mujeebks10 <mujeebks10@gmail.com>
2026-07-27 14:58:38 +0100

    modified scritps

--- scripts/s3DynamoDB.sh
+++ scripts/s3DynamoDB.sh
@@ -1,12 +1,14 @@
+#!/bin/bash
+
 # Create S3 buckets
 aws s3 mb s3://myapp01-terraform-state-dev --region us-east-1
-aws s3 mb s3://myapp01-terraform-state-staging --region us-east-1
-aws s3 mb s3://myapp01-terraform-state-prod --region us-east-1
+# aws s3 mb s3://myapp01-terraform-state-staging --region us-east-1
+# aws s3 mb s3://myapp01-terraform-state-prod --region us-east-1
 
 # Enable versioning
 aws s3api put-bucket-versioning --bucket myapp01-terraform-state-dev --versioning-configuration Status=Enabled
-aws s3api put-bucket-versioning --bucket myapp01-terraform-state-staging --versioning-configuration Status=Enabled
-aws s3api put-bucket-versioning --bucket myapp01-terraform-state-prod --versioning-configuration Status=Enabled
+# aws s3api put-bucket-versioning --bucket myapp01-terraform-state-staging --versioning-configuration Status=Enabled
+# aws s3api put-bucket-versioning --bucket myapp01-terraform-state-prod --versioning-configuration Status=Enabled
 
 # Create DynamoDB tables
 aws dynamodb create-table \
@@ -15,14 +17,14 @@ aws dynamodb create-table \
     --key-schema AttributeName=LockID,KeyType=HASH \
     --billing-mode PAY_PER_REQUEST
 
-aws dynamodb create-table \
-    --table-name myapp-terraform-locks-staging \
-    --attribute-definitions AttributeName=LockID,AttributeType=S \
-    --key-schema AttributeName=LockID,KeyType=HASH \
-    --billing-mode PAY_PER_REQUEST
+# aws dynamodb create-table \
+#     --table-name myapp-terraform-locks-staging \
+#     --attribute-definitions AttributeName=LockID,AttributeType=S \
+#     --key-schema AttributeName=LockID,KeyType=HASH \
+#     --billing-mode PAY_PER_REQUEST
 
-aws dynamodb create-table \
-    --table-name myapp-terraform-locks-prod \
-    --attribute-definitions AttributeName=LockID,AttributeType=S \
-    --key-schema AttributeName=LockID,KeyType=HASH \
-    --billing-mode PAY_PER_REQUEST
+# aws dynamodb create-table \
+#     --table-name myapp-terraform-locks-prod \
+#     --attribute-definitions AttributeName=LockID,AttributeType=S \
+#     --key-schema AttributeName=LockID,KeyType=HASH \
+#     --billing-mode PAY_PER_REQUEST
